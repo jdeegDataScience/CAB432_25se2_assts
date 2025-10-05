502f7c0f3e789bda9fd4344727f21304850ed7b5
JadeD <jadedeegan04@gmail.com>
2025-10-05 22:51:58 +1000

    added memcached to getpuzzles mw

--- expsokobansolver/middleware/getpuzzles.js
+++ expsokobansolver/middleware/getpuzzles.js
@@ -1,26 +1,40 @@
 require('dotenv').config();
+const { memch } = require("../../services/aws");
 
 /* GET movies search results. */
-module.exports = function(req, res, next) {
+module.exports = async function(req, res, next) {
     const selectCols = [
         'puzzleId', 'userId', 'name', 'cost', 'ts'
     ];
-    
-    req.db.from("puzzles").select(selectCols)
-    .where((builder) => {
-        // If NOT admin, filter by userId
-        if (!req.user.groups.includes("admins")) {builder.where("userId", parseInt(req.user.id));};
-    }).orderBy('ts')
-    .then((rows) => {
-        req.puzzles = rows;
-    })
-    .then(_ => {
-        next();
-    })
-    .catch((err) => { 
-        console.log(err);
-        res.json({Error: true, Message: "Error in MySQL query" });
-        return;
-    });
+    const cacheKey = (req.user.groups.includes("admins")) ? "admin" : req.user.id;
 
+    // Try to get from cache first
+    const cacheRes = await memch.aGet(`puzzles_${cacheKey}`)
+    .catch((err) => { 
+        console.log("Memcached get error: ", err);
+    });
+    if (cacheRes) {
+        console.log("Cache hit for puzzles");
+        req.puzzles = JSON.parse(cacheRes);
+        return next();
+    }
+    else {
+        const rows = await req.db.from("puzzles").select(selectCols)
+        .where((builder) => {
+            // If NOT admin, filter by userId
+            if (!req.user.groups.includes("admins")) {builder.where("userId", parseInt(req.user.id));};
+        }).orderBy('ts')
+        .catch((err) => { 
+            console.log(err);
+            res.json({Error: true, Message: "Error in MySQL query" });
+            return;
+        });
+        // Store in cache for next time
+        await memch.aSet(`puzzles_${cacheKey}`, JSON.stringify(rows), 300)
+        .catch((err) => {
+            console.log("Memcached set error: ", err);
+        });
+        req.puzzles = rows;
+        next();
+    };
 };
--- expsokobansolver/services/aws.js
+++ expsokobansolver/services/aws.js
@@ -2,11 +2,16 @@
 require("dotenv").config();
 const { S3Client } = require("@aws-sdk/client-s3");
 const { SecretsManagerClient } = require("@aws-sdk/client-secrets-manager");
-const { SESClient } = require("@aws-sdk/client-ses");
+// const { SESClient } = require("@aws-sdk/client-ses");
 // const { S3RequestPresigner } = require("@aws-sdk/s3-request-presigner");
 const { CognitoIdentityProviderClient } = require("@aws-sdk/client-cognito-identity-provider");
 const { CognitoJwtVerifier } = require("aws-jwt-verify");
 const { SSMClient } = require("@aws-sdk/client-ssm");
+const Memcached = require("memcached");
+const util = require("node:util");
+
+// Replace this with the endpoint for your Elasticache instance
+const memcachedAddress = "something.cfg.apse2.cache.amazonaws.com:11211";
 
 
 const jwtIdVerifier = CognitoJwtVerifier.create({
@@ -22,9 +27,18 @@ const region = process.env.AWS_REGION || 'ap-southeast-2';
 const s3 = new S3Client({ region });
 // const s3presigner = new S3RequestPresigner({ client: s3 });
 const secrets = new SecretsManagerClient({ region });
-const ses = new SESClient({ region });
+// const ses = new SESClient({ region });
 const cognito = new CognitoIdentityProviderClient({ region });
 const ssm = new SSMClient({ region });
+const memch = new Memcached(memcachedAddress);
+memch.on("failure", (details) => {
+    console.log("Memcached server failure: ", details);
+});
+
+// Monkey patch some functions for convenience
+// can call these with async
+memch.aGet = util.promisify(memch.get);
+memch.aSet = util.promisify(memch.set);
 
 
 module.exports = {
@@ -33,7 +47,8 @@ module.exports = {
     dynamo,
     secrets,
     cognito,
-    ses,
+    // ses,
     ssm,
+    memch,
     jwtIdVerifier
 };
