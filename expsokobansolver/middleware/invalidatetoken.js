79434567678f1464c1f934450ff5010143bc28c0
JadeD <jadedeegan04@gmail.com>
2025-10-06 15:22:31 +1000

    Made invalidatetoken mw agnostic of logic flow. Will accept userEmail from req.body OR req.user

--- expsokobansolver/middleware/invalidatetoken.js
+++ expsokobansolver/middleware/invalidatetoken.js
@@ -1,7 +1,8 @@
 module.exports = function(req, res, next) {
     const BLtime = Math.floor(Date.now() / 1000);
+    const userEmail = req.user.email ? req.user.email : req.body.email;
     req.db("usersbltokens")
-    .insert({email: req.user.email, blfrom: `${BLtime}`})
+    .insert({email: userEmail, blfrom: `${BLtime}`})
     .onConflict('email')
     .merge({blfrom: BLtime})
     .then(_ => {
