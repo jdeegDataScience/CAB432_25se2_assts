bbd19d1a5d83b09160f3aa710a0df4183481afc5
JadeD <jadedeegan04@gmail.com>
2025-10-06 14:41:43 +1000

    correct req.body.email to req.user.email

--- expsokobansolver/middleware/invalidatetoken.js
+++ expsokobansolver/middleware/invalidatetoken.js
@@ -1,7 +1,7 @@
 module.exports = function(req, res, next) {
     const BLtime = Math.floor(Date.now() / 1000);
     req.db("usersbltokens")
-    .insert({email: req.body.email, BLfrom: `${BLtime}`})
+    .insert({email: req.user.email, BLfrom: `${BLtime}`})
     .onConflict('email')
     .merge('BLfrom')
     .then(_ => {
