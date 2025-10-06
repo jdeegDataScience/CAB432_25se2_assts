aafedc0fad95b2e662bd5345b73e467b2eea2e99
JadeD <jadedeegan04@gmail.com>
2025-10-06 15:17:19 +1000

    corrected typo ref from 'res' to 'req'

--- expsokobansolver/middleware/usertokens.js
+++ expsokobansolver/middleware/usertokens.js
@@ -2,10 +2,10 @@ module.exports = async function(req, res, next) {
     // ID Tokens are used to authenticate users to your application
     const id_token = req.AuthRes.IdToken;
     // Access tokens are used to link IAM roles to identities for accessing AWS services
-    const access_token = res.AuthRes.AccessToken;
+    const access_token = req.AuthRes.AccessToken;
 
     // Refresh tokens are used to get new ID and access tokens
-    const refresh_token = res.AuthRes.RefreshToken;
+    const refresh_token = req.AuthRes.RefreshToken;
     
     res.status(200).json({
         bearerToken: {
