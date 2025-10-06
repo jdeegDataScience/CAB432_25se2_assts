35f9992f81d8fee67edc867c7a1580bc56cb5a58
JadeD <jadedeegan04@gmail.com>
2025-10-06 13:55:54 +1000

    knex config to correct searchPath

--- expsokobansolver/knexfile.js
+++ expsokobansolver/knexfile.js
@@ -7,6 +7,8 @@ module.exports = {
         port: process.env?.DB_PORT || 3306,
         database: process.env?.DB_NAME || "videos",
         user: process.env?.DB_USER || "root",
-        password: process.env.DB_PASSWORD
-    } 
+        password: process.env.DB_PASSWORD,
+        ssl: { rejectUnauthorized: false }
+    },
+    searchPath: [process.env.DB_USER, 'public']
 };
